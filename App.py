de3054612ea71ca95b0db421841a4df0d415e65b
VENKATA012 <bandivenkatadeepak@gmail.com>
2025-03-08 17:24:52 +0530

    Update App.py

--- App.py
+++ App.py
@@ -78,8 +78,8 @@ page = st.sidebar.radio("Go to", ["🏠 Home", "📖 About", "🔬 Lung Detectio
 
 # Home Page
 if page == "🏠 Home":
+     st.title("🏠 Welcome to Lung Cancer Detection System")
     st.image("digital-lung-anatomy-visualization-ai-generated-detailed-visualization-human-lungs-illuminated-vibrant-blue-orange-326767653.webp", use_column_width=True)
-    st.title("🏠 Welcome to Lung Cancer Detection System")
     st.write("Detect lung diseases using AI-powered deep learning models.")
     
     if st.button("🔬 Start Detection"):
